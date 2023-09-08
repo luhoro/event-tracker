8d2fdc1ea7dd738a8b1354bb7b552f5f7773b8a5
luhoro <luisashorowitz@gmail.com>
2023-09-08 14:37:42 -0300

    Build: Add recoil lib

---
+++ src/state/atom.ts
@@ -0,0 +1,20 @@
+import { atom } from 'recoil'
+import { IEvento } from '../interfaces/IEvento'
+
+export const listaDeEventosState = atom<IEvento[]>({
+  key: 'listaDeEventosState',
+  default: [{
+    descricao: 'Estudar React',
+    inicio: new Date('2023-09-09T09:00'),
+    fim: new Date('2023-09-09T10:30'),
+    completo: false,
+    id: 1642342747,
+  },
+  {
+    descricao: 'Estudar Recoil',
+    inicio: new Date('2023-09-08T13:00'),
+    fim: new Date('2023-09-08T14:00'),
+    completo: false,
+    id: 1642342959,
+  },]
+})
