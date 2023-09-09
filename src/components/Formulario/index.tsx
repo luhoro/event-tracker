c3ebc04f0d193c733ab5d0cdae686b1485dda33f
luhoro <luisashorowitz@gmail.com>
2023-09-09 11:56:48 -0300

    Feat: Add error messages for task register

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react'
-import { useSetRecoilState } from 'recoil'
-import { IEvento } from '../../interfaces/IEvento'
 import style from './Formulario.module.scss'
-import { obterId } from '../../utils'
-import { listaDeEventosState } from '../../state/atom'
+import useAdicionarEvento from '../../state/hooks/useAdicionarEvento'
 
 const Formulario: React.FC = () => {
-  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState)
+  const adicionaEvento = useAdicionarEvento()
 
   const [descricao, setDescricao] = useState('')
   const [dataInicio, setDataInicio] = useState('')
@@ -22,20 +19,22 @@ const Formulario: React.FC = () => {
   const submeterForm = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
 
-    const evento = ({
-      id: obterId(),
-      descricao,
-      inicio: montarData(dataInicio, horaInicio),
-      fim: montarData(dataFim, horaFim),
-      completo: false,
-    })
-
-    setListaDeEventos(listaAntiga => [...listaAntiga, evento])
-    setDescricao('')
-    setDataInicio('')
-    setHoraInicio('')
-    setDataFim('')
-    setHoraFim('')
+    try {
+      const evento = ({
+        descricao,
+        inicio: montarData(dataInicio, horaInicio),
+        fim: montarData(dataFim, horaFim),
+        completo: false,
+      })
+      adicionaEvento(evento)
+      setDescricao('')
+      setDataInicio('')
+      setHoraInicio('')
+      setDataFim('')
+      setHoraFim('')
+    } catch (error) {
+      alert(error)
+    }
   }
 
   return (
