db9488924a903bce8c65725c925317f4d490da0d
luhoro <luisashorowitz@gmail.com>
2023-09-10 18:38:52 -0300

    Refactor: Filter task

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,13 @@
-import { useState } from 'react'
 import style from './App.module.scss'
 import Card from './components/Card'
 import Formulario from './components/Formulario'
-import { IEvento } from './interfaces/IEvento'
 import Calendario from './components/Calendario'
 import ListaDeEventos from './components/ListaDeEventos'
 
 import { RecoilRoot } from 'recoil'
 
 function App() {
-  // const [eventos, setEventos] = useState<IEvento[]>([
-  //   {
-  //     descricao: 'Estudar React',
-  //     inicio: new Date('2023-09-09T09:00'),
-  //     fim: new Date('2023-09-09T10:30'),
-  //     completo: false,
-  //     id: 1642342747,
-  //   },
-  //   {
-  //     descricao: 'Estudar Recoil',
-  //     inicio: new Date('2023-09-08T13:00'),
-  //     fim: new Date('2023-09-08T14:00'),
-  //     completo: false,
-  //     id: 1642342959,
-  //   },
-  // ])
-
-  const [filtro, setFiltro] = useState<Date | null>()
-
-  const adicionarEvento = (evento: IEvento) => {
-    evento.id = Math.round(new Date().getTime() / 1000)
-    // // eventos.push(evento)
-    // setEventos([...eventos])
-  }
-  const alterarStatusEvento = (id: number) => {
-    // const evento = eventos.find((evento) => evento.id === id)
-  //   if (evento) {
-  //     evento.completo = !evento.completo
-  //   }
-  //   setEventos([...eventos])
-  }
-  const deletarEvento = (id: number) => {
-    // setEventos([...eventos.filter((evento) => evento.id !== id)])
-  }
-
-  const aplicarFiltro = (data: Date | null) => {
-    setFiltro(data)
-  }
-
-  // const filtrados = !filtro
-  //   ? eventos
-  //   : eventos.filter(
-  //       (evento) =>
-  //         filtro!.toISOString().slice(0, 10) ===
-  //         evento.inicio.toISOString().slice(0, 10)
-  //     )
-
+ 
   return (
     <RecoilRoot>
       <div className={style.App}>
@@ -65,9 +17,7 @@ function App() {
           </Card><hr />
 
           <Card>
-            <ListaDeEventos
-              aoFiltroAplicado={aplicarFiltro}
-            />
+            <ListaDeEventos/>
           </Card>
         </div>
         
